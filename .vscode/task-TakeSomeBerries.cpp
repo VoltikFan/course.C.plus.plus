8bb5a7ee023913910f5298ccde17592d7ac5a336
Ivan Kolisnyk <i.stahanov2000@gmail.com>
2024-01-09 18:42:47 +0100

    training task

---
+++ .vscode/task-TakeSomeBerries.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Masha, Misha, eated, ans;
+    cin>>Masha>>Misha>>eated;
+    ans = Misha + Masha - eated;
+    
+    if ( eated > Masha + Misha) cout<<"Impossible";
+    else cout<<ans;
+}
