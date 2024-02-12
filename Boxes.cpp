e368df20ef218970fd7656284e31c73843405b61
Ivan Kolisnyk <i.stahanov2000@gmail.com>
2024-02-12 18:37:00 +0100

    Fix

--- Boxes.cpp
+++ Boxes.cpp
@@ -4,8 +4,26 @@ using namespace std;
 
 int main()
 {
-    int mandarines, boxes, antwort;
-    cin>>mandarines>>boxes;
+    int mandarines, vmboxes, antwort;
+    cin>>mandarines>>vmboxes;
 
-    
+    if (vmboxes % 13 == 0){
+        vmboxes = vmboxes - 1;
+    }
+    if (vmboxes == 0){
+        cout<<"-1";
+        return 0;
+    }
+
+    antwort = mandarines / vmboxes;
+
+    if (mandarines % vmboxes != 0){
+        antwort = antwort + 1;
+    }
+
+    if (antwort % 13 == 0){
+        antwort = antwort + 1;
+    }
+
+    cout<<antwort;
 }
