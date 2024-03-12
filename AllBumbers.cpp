01f264b7dd9ec60f8975e8a25d89b12f371828c1
Ivan Kolisnyk <i.stahanov2000@gmail.com>
2024-03-12 20:08:46 +0100

    12.03.24

---
+++ AllBumbers.cpp
@@ -0,0 +1,23 @@
+
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, a = 0, b = 0, i = 1;
+    cin >> n;
+
+    while (i <= n) {
+        a = a + i;
+        if (a % 3 == 0) 
+        b = b + 1;
+        i = i + 1;
+    }
+    cout << b;
+    return 0;
+}
