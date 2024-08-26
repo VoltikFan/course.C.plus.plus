771ff61fb81dafe92818616c76e55504e976a20b
Ivan <i.stahanov2000@gmail.com>
2024-08-26 18:01:59 +0200

    26.08.24

---
+++ Birthday_day.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, k, ans, all_candies;
+    cin >> n >> k;
+
+    int d;
+
+    for (int i = 0; i <= n; i ++) {
+        cin >> d;
+        int v[d]++; 
+    }
+}
