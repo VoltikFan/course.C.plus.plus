1fa7a2ac57e207be55e2e00ad6fb76f79f9f9c50
Ivan <i.stahanov2000@gmail.com>
2024-11-04 17:59:44 +0100

    4.11.24

--- max_sum_3_numbers.cpp
+++ max_sum_3_numbers.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-using namespace tsd;
+using namespace std;
 
 int main()
 {
-    
+    int a, b, c, k;
+    int ant;
+    cin >> a >> b >> c >> k;
+
+    for (int i = 0; i < k; i ++) {
+        if (a >= b && a >= c) a *= 2;
+            else if (b >= a && b >= c) b *= 2;
+                else c *= 2;
+    }
+    ant = a + b + c;
+
+    cout << ant;
 }
