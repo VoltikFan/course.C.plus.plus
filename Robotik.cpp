9f0c31f1b7f66146896a7f46fb63a9c1f3d01c15
Ivan <i.stahanov2000@gmail.com>
2024-12-09 18:05:15 +0100

    9.12.24

--- Robotik.cpp
+++ Robotik.cpp
@@ -4,33 +4,31 @@ using namespace std;
 
 int main() 
 {
-    long long a, b, c;
+    long long a, b, c, ant;
     cin >> a >> b >> c;
 
-    long long total = a + b + c;
+    long long all = a + b + c;
 
-    if (total % 3 != 0) {
+    if (all % 3 != 0) {
         cout << -1;
         return 0;
     }
 
-    long long target = total / 3;
+    long long end_stan = all / 3;
 
-    if (a == target && b == target && c == target) {
+    if (a == end_stan && b == end_stan && c == end_stan) {
         cout << 0;
         return 0;
-    }
+    }   
+    long long a1, b1, c1, a2, b2, c2, zero = 0;
+    a1 = max(zero, a - end_stan);
+    b1 = max(zero, b - end_stan);
+    c1 = max(zero, c - end_stan);
+    a2 = max(zero, end_stan - a);
+    b2 = max(zero, end_stan - b);
+    c2 = max(zero, end_stan - c);
 
-    long long a1 = max(0ll, a - target);
-    long long b1 = max(0ll, b - target);
-    long long c1 = max(0ll, c - target);
-    long long a2 = max(0ll, target - a);
-    long long b2 = max(0ll, target - b);
-    long long c2 = max(0ll, target - c);
-
-    long long moves = max(a1, a2) + max(b1, b2) + max(c1, c2);
-
-    cout << moves / 2;
-
+    ant = (max(a1, a2) + max(b1, b2) + max(c1, c2)) / 2;
+    cout << ant;
     return 0;
 }
