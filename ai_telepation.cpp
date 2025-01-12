b5e2da6a9321b1dedd4c5fa3285e32e0185e41bb
Ivan <i.stahanov2000@gmail.com>
2025-01-12 18:15:55 +0100

    ai_teleportation_1

---
+++ ai_telepation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int a, b, n;
+    cin >> a >> b >> n;
+
+    for (int i = 0; i < n; i++) {
+        a++;
+        b = b + 2;
+        swap(a, b);
+    }
+    cout << a + b;
+}
