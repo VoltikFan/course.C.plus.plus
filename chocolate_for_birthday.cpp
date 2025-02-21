83dd42b01aadfc815fe6babbf06ff85df32d6d0b
Ivan <i.stahanov2000@gmail.com>
2025-02-21 22:01:16 +0100

    for_last_lesson

---
+++ chocolate_for_birthday.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long m, n, k;
+    cin >> m >> n >> k;
+
+    long long max_chocolate = 0;
+
+    for (long long i = 1; i <= m; i++) {
+        long long j = min(n, (m * n - k + 1) / i);
+        long long chocolate = i * j;
+        max_chocolate = max(max_chocolate, chocolate);
+    }
+
+    for (long long j = 1; j <= n; j++) {
+        long long i = min(m, (m * n - k + 1) / j);
+        long long chocolate = i * j;
+        max_chocolate = max(max_chocolate, chocolate);
+    }
+
+    cout << max_chocolate;
+
+    return 0;
+}
