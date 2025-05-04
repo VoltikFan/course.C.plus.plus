7c71d89b001915a3f9d35d41850c29d9d719e515
Ivan <i.stahanov2000@gmail.com>
2025-05-04 18:02:15 +0200

    04.05.25

---
+++ udachnaa_fotosesia.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<int> ant(m);
+    for (int j = 0; j < m; j++) {
+        int l, r, x;
+        cin >> l >> r >> x;
+
+        int b = 0;
+        for (int i = l - 1; i < r; i++) { 
+            if (a[i] == x + (i - (l - 1))) { 
+                b++;
+            }
+        }
+        ant[j] = b;
+    }
+
+    for (int i = 0; i < m; i++) {
+        cout << ant[i] << endl;
+    }
+
+    return 0;
+}
