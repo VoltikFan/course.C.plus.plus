46ea53c38956f6dfc3fe640f128a26657b26fc2f
Ivan <i.stahanov2000@gmail.com>
2025-05-05 18:12:57 +0200

    05.05.25-beautiful_date

---
+++ lunch_boxes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> k(m);
+    for (int i = 0; i < m; i++) {
+        cin >> k[i];
+    }
+
+    int ant = 0;
+
+    for (int i = 0; i < m; i++) {
+        if (n >= k[i]) { 
+            n -= k[i]; 
+            ant++; 
+        }
+    }
+
+    cout << ant;
+
+    return 0;
+}
---
+++ rivnania.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a + b == c) {
+        cout << a << "+" << b << "=" << c;
+        return 0;
+    }
+    if (a - b == c) {
+        cout << a << "-" << b << "=" << c;
+        return 0;
+    }
+    if (a * b == c) {
+        cout << a << "*" << b << "=" << c;
+        return 0;
+    }
+    if (a / b == c) { 
+        cout << a << "/" << b << "=" << c;
+        return 0;
+    }
+
+    if (a == b + c) {
+        cout << a << "=" << b << "+" << c;
+        return 0;
+    }
+    if (a == b - c) {
+        cout << a << "=" << b << "-" << c;
+        return 0;
+    }
+    if (a == b * c) {
+        cout << a << "=" << b << "*" << c;
+        return 0;
+    }
+    if (a == b / c) {
+        cout << a << "=" << b << "/" << c;
+        return 0;
+    }
+
+    return 0;
+}
