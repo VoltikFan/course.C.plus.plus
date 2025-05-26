efd38da168358724ca43f3d3adc4e60faf8ec04f
Ivan <i.stahanov2000@gmail.com>
2025-05-26 18:37:35 +0200

    26.05.25

---
+++ kuzya.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    long long n, one = 1;
+    cin >> n;
+
+    long long ant = 0;
+
+    for (long long i = n; i >= max(one, n - 100); i--) {
+        long long x = i;
+        long long a = 1;
+        while (x > 0) {
+            a *= x % 10;
+            x /= 10;
+        }
+        if (a > ant)
+            ant = a;
+    }
+
+    cout << ant;
+}
