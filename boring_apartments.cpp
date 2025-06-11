373af2e21b714de1530b977c68a8d683d2571eea
Ivan <i.stahanov2000@gmail.com>
2025-06-11 13:15:01 +0200

    11.06.25(2)

--- boring_apartments.cpp
+++ boring_apartments.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n, k;
+int main() 
+{
+    long long n, k;
     cin >> n >> k;
 
-    int d = k % 10; 
-    int a = 0, b = k;
-    while (b) {
-        a++;
-        b /= 10;
+    int ant = 0;
+
+    for (int d = 1; d <= 9; d++) {
+        long long j = d;
+        while (j <= n) {
+            ant++;
+            if (j == k) {
+                cout << ant;
+                return 0;
+            }
+            j = j * 10 + d;
+        }
     }
 
-    int result = (d - 1) * a; 
-    int j = 0;
-    for (int i = 1; i <= a; i++) {
-        j = j * 10 + d;
-        if (j <= k) result++;
-    }
-
-    cout << result;
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 
 int main() {
-    int n, k;
+    long long n, k;
     cin >> n >> k;
 
-    int d = k % 10; 
-    int len = 0;
-    int temp = k;
+    int count = 0;
 
-    while (temp > 0) {
-        len++;
-        temp /= 10;
+    for (int d = 1; d <= 9; ++d) {
+        long long num = d;
+        while (num <= n) {
+            count++;
+            if (num == k) {
+                cout << count;
+                return 0;
+            }
+            num = num * 10 + d;
+        }
     }
 
-    int result = (d - 1) * 10 + len;
-
-    cout << result;
     return 0;
 }
